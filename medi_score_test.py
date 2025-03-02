a1c3de397152686e3d71b863511b06588b80a8a3
Libbylsm <libbylsm05@gmail.com>
2025-03-02 23:47:11 +0000

    With CBG bonus task

--- medi_score_test.py
+++ medi_score_test.py
@@ -1,4 +1,5 @@
-def medi_score_calculation(air_or_oxygen, consciousness, respiration_rate, spo2, temperature):
+def medi_score_calculation(air_or_oxygen, consciousness, respiration_rate, spo2, temperature, cbg, fasting):
+# last two parameters are for the CBG bonus task
 
     medi_score = 0
 
@@ -62,20 +63,47 @@ def medi_score_calculation(air_or_oxygen, consciousness, respiration_rate, spo2,
     elif temperature >= 39.1:
         medi_score += 2
 
+    # CBG (capillary blood glucose) bonus task
+    if fasting: # if fasting
+        if cbg <= 3.4:
+            medi_score += 3
+        elif 3.5 <= cbg <= 3.9:
+            medi_score += 2
+        elif 4.0 <= cbg <= 5.4:
+            medi_score += 0
+        elif 5.5 <= cbg <= 5.9:
+            medi_score += 1
+        elif cbg >= 6.0:
+            medi_score += 3
+    else: # 2 hours after eating
+        if cbg <= 4.5:
+            medi_score += 3
+        elif 4.6 <= cbg <= 5.8: # table says 4.5 - 4.8 for score 2. This is assuming it means 4.6 - 5.8
+            medi_score += 2
+        elif 5.9 <= cbg <= 7.8:
+            medi_score += 0
+        elif 7.9 <= cbg <= 8.9:
+            medi_score += 2
+        elif cbg >= 9.0:
+            medi_score += 3
+
+
     return medi_score
 
 
-# Examples
-patient1 = medi_score_calculation(0, 0, 15, 95, 37.1)
-patient2 = medi_score_calculation(2, 0, 17, 96, 37.1)
-patient3 = medi_score_calculation(2, 1, 23, 88, 38.5)
+# Examples (added last two arguments for the bonus task)
+patient1 = medi_score_calculation(0, 0, 15, 95, 37.1, 3.5, True)
+patient2 = medi_score_calculation(2, 0, 17, 96, 37.1, 5.5, False)
+patient3 = medi_score_calculation(2, 1, 23, 88, 38.5, 4.5, True)
 
 
 # Test cases
+# Before bonus task (without cbg and fasting)
 print("Patient 1's medi score is:", patient1) # 0
 print("Patient 2's medi score is:", patient2) # 4
 print("Patient 3's medi score is:", patient3) # 8
 
+# Outputs are 2, 6, 8 after bonus task (adding cbg and fasting)
 
 
     
